39257239db4e16905d3e1653d0e4a5e925a2c452
ensfrknkc <ensfrknkc58@gmail.com>
2021-03-26 23:28:13 +0300

    Menu Table-Model Creadted

--- contents/models.py
+++ contents/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True,upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
