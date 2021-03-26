d6a64c711da9403e9905973e58c7bbb6673d5bf7
ensfrknkc <ensfrknkc58@gmail.com>
2021-03-27 00:28:24 +0300

    Created admin panel - Menu table shown

--- contents/admin.py
+++ contents/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from contents.models import Menu
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_filter = ['status']
+
+admin.site.register(Menu,MenuAdmin)
