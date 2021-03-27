750edb578c72f8075e08616250f83b104bb9d993
ensfrknkc <ensfrknkc58@gmail.com>
2021-03-27 20:09:41 +0300

    Content Model Created

--- contents/admin.py
+++ contents/admin.py
@@ -1,20 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from contents.models import Content
 from contents.models import Menu
-
 class MenuAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
 
-class ContentAdmin(admin.ModelAdmin):
-    list_display = ['title' , 'type', 'status']
-    list_filter = ['status' , 'type']
-
 admin.site.register(Menu,MenuAdmin)
-admin.site.register(Content,ContentAdmin)
-
-
-
-
