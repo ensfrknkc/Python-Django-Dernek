4f18306c349619bf777d1829bb826bea501f0bfe
Enes Furkan KOÇ <ensfrknkc58@gmail.com>
2021-05-02 00:28:22 +0300

    Contact Form created

--- home/views.py
+++ home/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
-
+from home.models import Setting, ContactFormu,ContactFormMessage
+from django.contrib import messages
 
 def index(request):
     setting = Setting.objects.get(pk=1)
@@ -20,7 +20,24 @@ def referanslar(request):
     context = {'setting': setting}
     return render(request, 'referanslarimiz.html', context)
 
+
+
 def iletisim(request):
+
+    if request.method == 'POST': # form post edildiyse
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage() # model ile bağlantı kur
+            data.name = form.cleaned_data['name'] #formdan bilgiti al
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save() #veritabanına kaydet
+            messages.success(request, "Mesajınız başarı ile gönderilmiştir. Teşekkür ederiz")
+            return HttpResponseRedirect('/iletisim')
+
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting}
+    form = ContactFormu()
+    context = {'setting': setting,'form':form}
     return render(request, 'iletisim.html', context)
